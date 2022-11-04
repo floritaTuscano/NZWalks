2a46f394eb8b1a69812a78a0bc791e820ec6287d
FloritaTuscano <florstuscano@gmail.com>
2022-11-04 18:26:00 +0530

    End of Section 2

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<NZWalksDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalsConnection"));
+
+});
 
 var app = builder.Build();
 
